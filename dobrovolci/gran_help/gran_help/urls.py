9e44cc05eeb409d01debbca764de4128840d6550
Airis Yasenova <airisyasenova@Airiss-MacBook-Pro.local>
2025-03-20 13:17:27 +0200

    fixed bugs

--- dobrovolci/gran_help/gran_help/urls.py
+++ dobrovolci/gran_help/gran_help/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for learning_users project.
+URL configuration for gran_help project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
