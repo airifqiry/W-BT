a82050ed699fa63fe8a97cde07a924fe65fe16c3
Airis Yasenova <airisyasenova@Airiss-MacBook-Pro.local>
2025-03-20 12:55:23 +0200

    settings change

--- dobrovolci/helpagramp/helpagramp/settings.py
+++ dobrovolci/helpagramp/helpagramp/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR=os.path.join(BASE_DIR,'templates')
+STATIC_DIR=os.path.join(BASE_DIR,'static')
+MEDIA_DIR=os.path.join(BASE_DIR,'media')
 
 
 # Quick-start development settings - unsuitable for production
