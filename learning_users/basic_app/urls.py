da97ca8b5360edb9cdc9837cbdc04c18f61343df
Airis Yasenova <airisyasenova@Airiss-MacBook-Pro.local>
2025-03-21 13:27:59 +0200

    fix

--- learning_users/basic_app/urls.py
+++ learning_users/basic_app/urls.py
@@ -4,7 +4,7 @@ from basic_app import views
 app_name = 'basic_app'
 
 urlpatterns = [
-    path('register/patient/', views.register_patient, name='register_patient'),  # нова
-    path('register/volunteer/', views.register_volunteer, name='register_volunteer'),  # нова
+    path('register_patient/', views.register_patient, name='register_patient'),  # нова
+    path('register_volunteer/', views.register_volunteer, name='register_volunteer'),  # нова
     path('user_login/', views.user_login, name='user_login'),
 ]
